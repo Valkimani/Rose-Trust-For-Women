ee14c63b4b1012e19b577c684cbfa0685c24c835
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-04-07 20:52:10 -0400

    Created a PORT for the server to run on and added a listen for the PORT in server.js file.

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require("express");
 // instance of express
-const app = require ("express")
+const app = require ("express");
+// PORT for the server to run on
+const PORT = process.env.PORT || 3000;
+// listen on PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
